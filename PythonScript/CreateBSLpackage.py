35fa4ffb9b8d553c91b02b6d205ab5706ab8454e
khaled_alhoz <kh.houz@gmail.com>
2020-11-24 19:17:23 +0100

    Solved many issues, now this script should be able to handel most situation of publishing Scene Layer

--- PythonScript/CreateBSLpackage.py
+++ PythonScript/CreateBSLpackage.py
@@ -19,10 +19,10 @@ from   datetime   import datetime
 def CreateBSLpackage(workSpaceEnv = None, GDBfolder_name    = r"AutomationGDB.gdb",\
 	out_FeatureDataset= r"Building_A"   , spatial_reference = r"RD New", \
 	Rvt_directory     = None            , BSL_name          = r"BSLpackage.slpk",\
-	nameOfBuildingL   = r"BuildL_A"     , deleteDirectory   = False,\
-	includeDate       = False):
+	nameOfBuildingL   = r"BuildL_A"     , includeDate       = False):
     
-    """ 
+	
+	""" 
 	Function 1: CreateBSLpackage():
 	This function creates BSL packages from a local revit folders
 	and it store it in a desired local folder
@@ -76,28 +76,39 @@ def CreateBSLpackage(workSpaceEnv = None, GDBfolder_name    = r"AutomationGDB.gd
 	if includeDate:
 		now = datetime.now()
 		day = now.strftime("%Y%m%d")
-		BSL_name = BSL_name[:-5] +day + BSL_name[-5:]
+		BSL_name = BSL_name[:-5] + day + BSL_name[-5:]
 	print (" Next: Creating BSL package with name ({})".format(BSL_name))
 	slpk = arcpy.CreateBuildingSceneLayerPackage_management(nameOfBuildingL, BSL_name, spatial_reference)
-	print ("Finsihed creating the following Building Scene Layer package {}".format(BSL_name))
+	print ("Finsihed creating the following Building Scene Layer package {}".format(slpk))
 	
-	arcpy.env.workspace = None
-	print (slpk)
 	return 
 
-def publishBSLfunction(itemID_or_titleOf_BSLp = None, dictOfPackageLayer = {}, DirectoryTo_SLPK = None, BSL_name=None ):
+def publishBSLfunction(itemID_BSLp = None, itemID_Hosted=None, dictOfPackageLayer = {}, DirectoryTo_SLPK = None, BSL_name="Name Not provided" ):
 
 	""" in this part of the code we will be accessing Web GIS and publish it into ArcGIS Online"""
 
 	gis = GIS("pro") # accessing ArcGIS online 
 	print ("Logged in as {}".format(gis.properties.user.username))
-	print ()
-
 	
-	itemPackage = gis.content.get(itemID_or_titleOf_BSLp)
-	if not itemPackage: raise KeyError("There is no item of type:Scene Package found with the provided ItemID")
-	itemHosted  = gis.content.search(query=itemPackage.title, item_type="Scene Layer")
-	print (itemHosted, itemPackage["id"])
+	
+	if itemID_BSLp is None:
+		print ("There is no itemID of type:Scene Package provided")
+		print ("Uploading BSL Package....")
+		slpk_item = gis.content.add(item_properties = dictOfPackageLayer, data= DirectoryTo_SLPK,\
+		 folder='packages')
+		print ("Publishing the updloaded BSL package.....")
+		slpk_published = slpk_item.publish()
+		print (r"work done")
+		return
+
+	itemPackage = gis.content.get(itemID_BSLp)
+	if itemID_Hosted is None:
+		itemHosted  = gis.content.search(query=itemPackage.title, item_type="Scene Layer")
+	else:
+		itemHosted  = gis.content.get(itemID_Hosted) 
+		itemHosted  = [itemHosted]
+	print (itemHosted, itemPackage)
+
 	if len(itemHosted) > 0:
 		for itemL in itemHosted:
 			if itemL["title"] == itemPackage["title"]:
@@ -105,10 +116,10 @@ def publishBSLfunction(itemID_or_titleOf_BSLp = None, dictOfPackageLayer = {}, D
 		if type (itemHosted) is list: 
 			print ("Couldn't find the correct (Hosted) item: many items holds the same title")
 			return None
-		print ("Found the following Layers \n1 - {} \n2 - {}".format(itemPackage, itemHosted ))
+		print ("Working on the following Layers \n1 - {} \n2 - {}".format(itemPackage, itemHosted ))
 
-		print ("Updating the following BSL Package ({})....".format(BSL_name[:-4]))
-		slpk_item = gis.content.add(item_properties = dictOfPackageLayer, data= DirectoryTo_SLPK, folder='packages')
+		print ("Updating BSL Package....")
+		slpk_item = gis.content.add(item_properties= dictOfPackageLayer, data= DirectoryTo_SLPK, folder='packages')
 
 		# get a string of todays data
 		now = datetime.now()
@@ -120,32 +131,46 @@ def publishBSLfunction(itemID_or_titleOf_BSLp = None, dictOfPackageLayer = {}, D
 			print ("Publishing the updated BSL package.....")
 			slpk_published = slpk_item.publish()
 			print (itemHosted["id"], slpk_published["id"])
-			print ()
+			checker = False
 			try:
-				checker = arcpy.server.ReplaceWebLayer(itemHosted["id"], "Archive_" + itemHosted ["title"] , slpk_published["id"], "KEEP", "TRUE")
+				checker = arcpy.server.ReplaceWebLayer(itemHosted["id"], "Archive_" + itemHosted ["title"] + day , slpk_published["id"], "KEEP", "TRUE")
 			except:
-				checker = arcpy.server.ReplaceWebLayer(itemHosted["id"], "Archive_" + itemHosted ["title"] +"__2", slpk_published["id"], "KEEP", "TRUE")
-			if checker: 
-				print ("Hosted Layer Scuccesfully replaced")
-			else:
-				print ("Something went wrong, Hosted Layer was not replaced")
+				print ()
+				print ("Replacement didn't work, trying with different Name")
+			if not checker:
+				try:
+					checker = arcpy.server.ReplaceWebLayer(itemHosted["id"], "Archive_" + itemHosted ["title"] + day + "__2", slpk_published["id"], "KEEP", "TRUE")
+					print ("Hosted Layer Scuccesfully replaced")
+				except:
+					print ("Replacement didn't work")
+			if not checker:
+				slpk_published.delete()
+
 		except:
-			print ("(error): Try to delete the following layer {} of Type \"Hosted Service\" ".format(slpk_item["title"]))
+			print ("(error): \nTry to delete the following layer {} of Type \"Hosted Service\" ".format(slpk_item["title"]))
+			print ("Or run publishBSLfunction () separetely")
 		slpk_item.update(item_properties= {"title": slpk_item["title"][:-len(day)]})
 		print (r"work done")
+
 	else:
+		print ("Updating BSL Package....")
 		slpk_item = gis.content.add(item_properties = dictOfPackageLayer, data= DirectoryTo_SLPK,\
 		 folder='packages')
+		print ("Publishing the updated BSL package.....")
 		slpk_published = slpk_item.publish()
 		print (r"work done")
 		
 	
 
-# Helper Function 
+# Helper Function to remove gdb folder when wished 
 def removeDirectory(PathToFolder):
 		return shutil.rmtree(PathToFolder)
 
 if __name__ == '__main__':
+
+	run_CreateBSLpackage  = True
+	run_publishBSLfunction= True
+	deleteDirectory       = False # only works if you run the second function seperately 
 	
 	"""required parameters and Optional parameters for the workflow
 	this workflow is devided into two main parts (here functions)
@@ -154,50 +179,42 @@ if __name__ == '__main__':
 	                   ###Function 1 CreateBSLpackage()#
 	                   #################################
 	"""
-	run_CreateBSLpackage  = True
-	run_publishBSLfunction= False
+
 	# Required
 	########## 
 	workSpaceEnv      =  r"C:\Users\alhoz\Desktop\AutomationTest" # path to a folder to createGDB
-	Rvt_directory     =  r"C:\Users\alhoz\Desktop\Automation\DATA\Berging.rvt" # path to rvt file 
-	BSL_name          =  r"BSLpackageTest.slpk" # BSLpackage Name defaul is **BSLpackage.slpk**
+	Rvt_directory     =  r"C:\Users\alhoz\Desktop\AutomationTest\revitFiles\171025_BLOKA.rvt" # path to rvt file 
+	BSL_name          =  r"BSLpackageBlockNew.slpk" # BSLpackage Name defaul is **BSLpackage.slpk**
 	spatial_reference =  r"RD New" #default is "RD New"
-	nameOfBuildingL   =  r"BuildL_A" #this will show on the ArcGIS online 
+	nameOfBuildingL   =  r"BuildL_Anew" #this will show on the ArcGIS online 
 	
 	# optional
 	########## 
 	out_FeatureDataset= r"Building_A"
-	GDBfolder_name    = r"AutomationTEST.gdb" #default 
-	deleteDirectory   = True
-	includeDate       = True 
+	GDBfolder_name    = r"AutomationTESTnew.gdb" #default
+	includeDate       = False 
 	####################################
 	# run the Function :CreateBSLpackage
 	if run_CreateBSLpackage:
 		CreateBSLpackage(workSpaceEnv, GDBfolder_name       , \
-		       out_FeatureDataset, spatial_reference    , Rvt_directory, BSL_name,\
-			   nameOfBuildingL   , deleteDirectory      , includeDate )
+			       out_FeatureDataset, spatial_reference    , Rvt_directory, BSL_name,\
+				   nameOfBuildingL   , includeDate )
 
-	# Helper function 
-	out_gdb_path    = workSpaceEnv + "\\" + GDBfolder_name
-	if deleteDirectory:
-		try:
-			removeDirectory(out_gdb_path)
-			print ("The folowing gdb file is removed{}".format(out_gdb_path, format_spec))
-		except:
-			print ("Failed Deleting GDB Folder")
 
-			#####################################################################
-			#####################################################################
-			#####################################################################
+
+	#####################################################################
+	#####################################################################
+	#####################################################################
 	"""                ###################################
 	                   ###Function 2 publishBSLfunction()#
 	                   ###################################
 	"""
 	# Required
 	##########
-	itemID_or_titleOf_BSLp  = "feaa22628f164732a2f7129214f395a2"
+	itemID_BSLp  = "4e27fbe2f642458b8108c41881e4bc9f" #"feaa22628f164732a2f7129214f395a2"
+	itemID_Hosted= "0af1fd92f95d46768a72b59845175bae"
 	# overwrite parameter is importatnt to be set on **True** ##Only change if you know what you are doing##  
-	dictOfPackageLayer      = {"overwrite" : True, "tags" : ["ArcGIS", "BuildingSceneLayer", "revit", "slpk", "SceneService"],\
+	dictOfPackageLayer      = {"overwrite" : True, "tags" : ["ArcGIS", "BuildingSceneLayer", "revit"],\
 					"snippet": "This is a Scene Package Created for testing the Automation process of uploading revit data"}
 
 	#### this parameter is automaticlly derived from the arguments of function 1 
@@ -208,4 +225,15 @@ if __name__ == '__main__':
 		BSL_name = BSL_name[:-5] +day + BSL_name[-5:]  
 	DirectoryTo_SLPK    = workSpaceEnv + "\\" + BSL_name
 	if run_publishBSLfunction:
-		publishBSLfunction(itemID_or_titleOf_BSLp, dictOfPackageLayer, DirectoryTo_SLPK, BSL_name)
+		publishBSLfunction(itemID_BSLp, itemID_Hosted, dictOfPackageLayer, DirectoryTo_SLPK, BSL_name)
+
+
+	# Helper function 
+	out_gdb_path    = workSpaceEnv + "\\" + GDBfolder_name
+	if deleteDirectory:
+		try:
+			print ("removing gdb files")
+			removeDirectory(out_gdb_path)
+			print ("The folowing gdb file is removed{}".format(out_gdb_path, format_spec))
+		except:
+			print ("Failed Deleting GDB Folder")
